df97fbb6ac29b06e15e3c2c0da13b14075cd6602
VictorSilva1974 <emersonvictor1974@gmail.com>
2023-09-25 20:16:00 -0300

    meu segundo commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// Defina o nome e a quantidade de experiência (XP) do herói
+let nome = "Emerson Victor"; // Substitua por um nome real
+let xp = 5000; // Substitua por uma quantidade real de XP
+
+// Use uma estrutura de decisão para determinar o nível do herói
+let nivel;
+
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp <= 5000) {
+    nivel = "Prata";
+} else if (xp <= 7000) {
+    nivel = 
+    nivel =
+
+    nivel
+"Ouro";
+} else if (xp <= 8000) {
+    nivel = 
+   
+"Platina";
+} else if (xp <= 9000) {
+    nivel = 
+    nivel
+
+   
+"Ascendente";
+} else if (xp <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = 
+    nivel =
+"Radiante";
+}
+
+// Exiba a mensagem com o nome e o nível do herói
+console.log("O Herói de nome " + nome + " está no nível de " + nivel);
